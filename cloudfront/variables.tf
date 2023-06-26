10561fa8da49e95f5c10fd28b92ec065afab124d
Juliano Kessler <juliano.kessler@viapath.com>
2023-06-26 10:54:40 -0300

    adding files 2

---
+++ cloudfront/variables.tf
@@ -0,0 +1,20 @@
+variable "domain" {
+   default = "gtldev.net"
+}
+
+variable "aws_region" {
+   default = "us-east-2"
+}
+
+variable "tf_bucket" {}
+
+variable "acm_arn_us_east-1" {}
+
+variable "bucket_namex" {
+   default = "my-viapath3"
+}
+
+variable "environment" {
+   default = "staging"
+}
+
